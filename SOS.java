5b183786b29f5a21f5a04f4c319e446bddeccbcd
Anne Apiyo <anneapiyo0@gmail.com>
2024-03-12 10:15:45 +0300

    Add files via upload

---
+++ SOS.java
@@ -0,0 +1,12 @@
+//import from the API
+import javax.swing.JFrame;
+
+public class SOS extends JFrame{
+    public static void main(String[] args) {
+        //create an instance of SOS
+        SOS myWing=new SOS();
+        myWing.setVisible(true);
+        myWing.setSize(200, 600);;
+    }
+
+}
